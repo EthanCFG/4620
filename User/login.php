31f5223a7b7869d513e22c9898906234da3a7a5e
Ethan <eaknigh@clemson.edu>
2021-11-19 23:55:07 -0500

    Modi

--- User/login.php
+++ User/login.php
@@ -1,5 +1,5 @@
 <?php
-  include_once "userHeader.php";
+  include_once "../header.php";
 ?>
 
 <!DOCTYPE html>
--- header.php
+++ header.php
@@ -20,12 +20,14 @@
           <?php
             if (isset($_SESSION["useruid"])) {
               echo "<li><a href='../index.php'>Home</a></li>";
+              echo "<li><a href='videoPlay.php'>Top Video</a></li>";
               echo "<li><a href='User/profile.php'>Profile Page</a></li>";
               echo "<li><a href='filemanager.php'>Upload Media</a></li>";
               echo "<li><a href='User/logout.php'>Logout</a></li>";
             }
             else {
               echo "<li><a href='index.php'>Home</a></li>";
+              echo "<li><a href='videoPlay.php'>Top Video</a></li>";
               echo "<li><a href='User/signup.php'>Sign up</a></li>";
               echo "<li><a href='User/login.php'>Log in</a></li>";
             }
