fe95cb09593752185f04233558dae936bd3c2d9d
Ethan <eaknigh@clemson.edu>
2021-11-19 23:56:26 -0500

    Last items

---
+++ videoPlay.php
@@ -0,0 +1,9 @@
+<?php
+  include_once "header.php";
+?>
+
+<section class="video-player">
+    <video width="320" height="240" controls>
+    <source src="/uploads/placeholder.mp4" type="video/mp4">
+    </video>
+</section>
