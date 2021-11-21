62491637c4b472f40ccd09a08d8a3074ca0d01f2
Ethan <eaknigh@clemson.edu>
2021-11-21 13:16:03 -0500

    jfjdsf

--- videoPlay.php
+++ videoPlay.php
@@ -4,6 +4,6 @@
 
 <section class="video-player">
     <video width="320" height="240" controls>
-    <source src="/uploads/placeholder.mp4" type="video/mp4">
+    <source src="uploads/placeholder.mp4" type="video/mp4">
     </video>
 </section>
